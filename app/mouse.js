bb7782efa93fd4b27839e6010bf8c9b2c0b78995
Asiulka1442 <asia-kuta@wp.pl>
2015-09-10 12:48:00 +0200

    exercise9

--- app/mouse.js
+++ app/mouse.js
@@ -1,3 +1,42 @@
 'use strict';
 
 var app = angular.module('app', []);
+
+
+app.directive("enter", function(){
+	return function(scope, element, attrs){
+		console.log(attrs)
+		element.bind("mouseenter", function(){
+			element.addClass(attrs.enter);
+		});
+
+	};
+});
+
+
+app.directive("leave", function() {
+    return function(scope, element, attrs) {
+        element.bind("mouseleave", function() {
+            element.removeClass(attrs.enter);
+        });
+    };
+});
+
+app.directive("clickOn", function(){
+	return function(scope, element, attrs){
+		console.log(attrs)
+		element.bind("click", function(){
+			element.addClass(attrs.clickOn);
+		});
+
+	};
+});
+
+
+app.directive("dbClickOn", function() {
+    return function(scope, element, attrs) {
+        element.bind("dblclick", function() {
+            element.removeClass(attrs.clickOn);
+        });
+    };
+});
