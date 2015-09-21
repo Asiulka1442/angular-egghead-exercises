01b7c9b6c61502465001f93d06262294409c7cd2
Asiulka1442 <asia-kuta@wp.pl>
2015-09-21 14:23:13 +0200

    exercise12

--- app/shop.js
+++ app/shop.js
@@ -28,9 +28,15 @@ app.controller('AppCtrl', function ($scope)
 app.directive('orderComponent', function ()
 {
     var template = '<div class="panel panel-success text-center"><p class="panel-heading"><strong>I want to buy:</strong></p><div class="panel-body">' +
-            '<select class="form-control form-group"><!--select element--></select>' +
-            '<div id="buyButton" class="btn btn-success"><!--the bottom div-->Buy!</div></div></div>';
+            '<select class="form-control form-group" ng-model="myProduct" ng-options="product.name for product in products"></select>' +
+            '<div id="buyButton" class="btn btn-success" ng-click="buy({foo:myProduct.name})">Buy!</div></div></div>';
     return {
+        restrict: 'E',
+            scope:{
+                products:"=",
+                buy:"&"
+
+            },
         template: template
     };
 });
