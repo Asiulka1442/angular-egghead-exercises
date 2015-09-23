e4273ac2075e7c4b1ec5028bbe344bd730278196
Asiulka1442 <asia-kuta@wp.pl>
2015-09-23 10:14:22 +0200

    exercise13

--- app/snow.js
+++ app/snow.js
@@ -6,8 +6,23 @@ app.controller('SnowCtrl', function ($scope)
 {
     $scope.snow = [];
 
-//    addPetal function
-//    removePetal function
+    $scope.addPetal = function (){
+    	$scope.snow.push("*")
+    	// return $scope.snow;
+    }
+
+    $scope.removePetal = function(){
+    	$scope.snow.pop()
+    	// return $scope.snow;
+    	}
 });
 
-//    add enter directive
+app.directive('enter', function (){
+	return function(scope, element, attrs){
+		element.bind('mouseenter', function(){
+			scope.$apply(attrs.enter)
+		})
+	}
+
+
+})
